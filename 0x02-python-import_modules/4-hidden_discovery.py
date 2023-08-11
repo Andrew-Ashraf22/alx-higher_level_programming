533efb859b715f572d12cbdf1909a7c9744505c8
Andrew-Ashraf22 <androa45@yahoo.com>
2023-08-11 10:16:24 -0700

    task 4 up

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-     import hidden_4
-     str = dir(hidden_4)
-     for i in str:
-         if i[0] != '_' and i[1] != '_':
-             print(i)
+    import hidden_4
+    str = dir(hidden_4)
+    for i in str:
+        if i[0] != '_' and i[1] != '_':
+            print(i)
