7f5383674b53eaef208913d4a219fec577658e59
Andrew-Ashraf22 <androa45@yahoo.com>
2023-08-16 12:28:54 -0700

    task 10 up

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,4 @@
 def best_score(a_dictionary):
     if a_dictionary == None:
         return (None)
-    return max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
+    return max(zip(a_dictionary, key= lambda x: a_dictionary[x]))
