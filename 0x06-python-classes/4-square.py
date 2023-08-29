20e1adc125352c64002505f62e458bdc13644faa
Andrew-Ashraf22 <androa45@yahoo.com>
2023-08-29 13:23:34 -0700

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""making a square class"""
+
+
+class Square:
+    """a square class"""
+    def __init__(self, size=0):
+        """make a square
+        Args:
+            size (int): size of square
+        """
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Return the area of a square"""
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """return size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
