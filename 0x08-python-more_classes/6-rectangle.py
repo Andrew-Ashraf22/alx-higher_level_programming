33cbfaf63b417ed9ffbd8f94f343282f4ee2bd4e
Andrew-Ashraf22 <androa45@yahoo.com>
2023-09-04 13:20:47 -0700

    task 6 up 2

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -3,7 +3,11 @@
 
 
 class Rectangle:
-    """ a rectangle class"""
+    """ a rectangle class
+        
+        Attributes:
+            number_of_instances (int): number of instances
+    """
     
     number_of_instances = 0
 
