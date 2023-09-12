31ff30382b139b17a9b74791b1f19885990921bc
Andrew-Ashraf22 <androa45@yahoo.com>
2023-09-12 14:53:50 -0700

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""make a function function that returns a dic of a simple
+data struct"""
+
+
+def class_to_json(obj):
+    """Return the dict of a simple data struct"""
+    def class_to_json(obj):
+    json = {}
+    attr = obj.__dict__
+    for key, value in attr.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            json[key] = value
+    return json
