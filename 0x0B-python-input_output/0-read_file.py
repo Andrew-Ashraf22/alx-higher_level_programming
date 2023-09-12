189c0b5c6c7f1f009b4e1892849eadb1eb428223
Andrew-Ashraf22 <androa45@yahoo.com>
2023-09-12 14:05:37 -0700

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""make a function that reads from file and print it to stdout"""
+
+
+def read_file(filename=""):
+    """
+        reads content of filename and prints them
+
+        Args:
+            filename (file): the file
+    """
+    with open(filename, mode= 'r', encoding = "utf-8") as _file:
+        print(_file.read())
