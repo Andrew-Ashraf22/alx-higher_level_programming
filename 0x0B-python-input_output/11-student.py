26f2a4167818f3d0dcd37a870998d9bd44d82aee
Andrew-Ashraf22 <androa45@yahoo.com>
2023-09-12 15:06:56 -0700

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""make a student class"""
+
+
+class Student:
+    """a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """make a new student
+
+        Args:
+            first_name (str): The first name
+            last_name (str): The last name
+            age (int): The age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dict reps of a student
+
+            Args:
+            attrs (list): The attr
+        """
+        if attrs is None:
+            return self.__dict__
+
+        json = {}
+        for key in attrs:
+            if hasattr(self, key):
+                json[key] = getattr(self, key)
+        return json
+
+    def reload_from_json(self, json):
+        """Replace all attrs in a student
+
+        Args:
+            json (dict): The dict to replace the attrs
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
