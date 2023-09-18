98cb2fb74996bf614f9d15fc88d11fc66549ce37
Andrew-Ashraf22 <androa45@yahoo.com>
2023-09-18 11:23:44 -0700

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,7 +14,7 @@ class Base:
             Args:
                 id (int): the id
         """
-         if id is not None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
